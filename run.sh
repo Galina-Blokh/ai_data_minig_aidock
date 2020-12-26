f9c6ba951c5cb39722ab5b71b8fc62b39c327d1f
Galina-Blokh <64019563+Galina-Blokh@users.noreply.github.com>
2020-12-26 15:33:10 +0200

    changed run.sh to get argpass

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-url_to_get=https://www.loveandlemons.com/mashed-cauliflower-recipe/
-python3 for_one_link_run.py "$url_to_get"
+url_to_get=$1
+python3 ./for_one_link_run.py "$url_to_get"
