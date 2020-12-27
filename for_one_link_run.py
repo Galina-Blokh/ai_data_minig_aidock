da64b1a9c0dafb78b763cdc79de0399eaf36bd60
Galina-Blokh <galinablokh0307@gmail.com>
2020-12-27 19:00:42 +0200

    1.added new TODOs into for_one_link_run.py 2. refreshed data folder with fresh and new data files

--- for_one_link_run.py
+++ for_one_link_run.py
@@ -9,13 +9,16 @@ def main_for_one_link():
     parser.add_argument('link')
     args = parser.parse_args()
 
-    g = get_recipe(args.link)
-    print_json(str(args.link).strip(), g)
+    dict_file = get_recipe(args.link)
+    # TODO clean-preprocess data from link
+    # TODO load pre-trained model
+    # TODO predict on new set
+    print_json(str(args.link).strip(), dict_file)
+    # TODO print to file/console info about prediction
 
 
 if __name__ == '__main__':
     main_for_one_link()
     # link = 'https://www.loveandlemons.com/focaccia/'
-    #
-
-# TODO here will be run a prediction and print for test set/link
+    # TODO add profile
+    # TODO add logging
