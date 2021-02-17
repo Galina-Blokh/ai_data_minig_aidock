dd2034f9e8166a648ece84b10a4504802a58e1ee
gal <galinablokh0307@gmail.com>
2021-02-17 19:53:45 +0200

    deleted regex constants deleted constants for LSTM model

--- config.py
+++ config.py
@@ -6,21 +6,14 @@ LOG_FILE = 'recipes_logging.log'
 TEST_LINKS_FILE = 'test_links.txt'
 DATA_FILE = 'new_recipe.pkl'
 
-# constants  for preprocess  data
-DIGIT_RX = "(\d+([\.|,]\d+)?[\w]*[\s|-])|[^A-Za-z\,()\.'\-: ]{1,7}"
-SYMBOL_RX = "[/(/)\-/*/,]|[^ -~]"
-DOT_RX = "\.{1,4}|\:"
-
 # for model , preprocess, training
 TEST_SIZE = 0.2
-BATCH_SIZE = 32
-EPOCHS = 200
-THRESHOLD = 0.5
+BATCH_SIZE = 16
+EPOCHS = 5
+# THRESHOLD = 0.5
 EMBEDDING_DIM = 32
-TRAIN_DATA_CLEAN = '/data/new_train_data_clean.pkl'
-TEST_DATA_CLEAN = '/data/new_test_data_clean.pkl'
-MODEL_NAME = '/data/my_model-12.h5'
+TRAIN_DATA_CLEAN = '/data/train_data_clean.pkl'
+TEST_DATA_CLEAN = '/data/test_data_clean.pkl'
+MODEL_NAME = '/data/distilbert.h5'
 MAX_SEQ_LEN = 217
-VOCAB_SIZE = 2474  # it is changed in hand way. It depends on scrapping part -
-# links to scrap each time goes in different order --> when you take out first 10 links
-# it will be 10 different links each scraper run --> the vocabulary every time will be different size
+
